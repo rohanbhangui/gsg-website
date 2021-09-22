8fbe7d0fc09bce3f5f6f716cc3c7e2346ab77c38
Rohan Bhangui <bhanguirohan@gmail.com>
2021-09-22 12:16:40 -0400

    fixed logos and order

--- src/pages/incubation/incubation.js
+++ src/pages/incubation/incubation.js
@@ -16,22 +16,6 @@ import NPC_HERO from "../../assets/img/npc-masterImage.png";
 
 const properties = [
   {
-    id: "national-players-club",
-    title: "National Players Club",
-    subtitle: "Lorem Ipesum",
-    logo: NPC,
-    description: `Koia understood the importance of having the right partner to represent their growth and prominence as a plant-based protein drink.
-
-    See how we helped Koia realize their potential when they linked up with NBA superstar Chris Paul. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
-    link: {
-      url: "https://national-players-club.myshopify.com/",
-      label: "Learn More",
-    },
-    masterImage: NPC_HERO
-  },
-  {
     id: "the-crew-league",
     title: "The Crew League",
     subtitle: "Lorem Ipesum",
@@ -46,6 +30,22 @@ const properties = [
       label: "Learn More",
     },
     masterImage: TCL_HERO
+  },
+  {
+    id: "national-players-club",
+    title: "National Players Club",
+    subtitle: "Lorem Ipesum",
+    logo: NPC,
+    description: `Koia understood the importance of having the right partner to represent their growth and prominence as a plant-based protein drink.
+
+    See how we helped Koia realize their potential when they linked up with NBA superstar Chris Paul. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+    link: {
+      url: "https://national-players-club.myshopify.com/",
+      label: "Learn More",
+    },
+    masterImage: NPC_HERO
   }
 ]
 
